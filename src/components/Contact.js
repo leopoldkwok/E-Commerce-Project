de84e5395f08e60ac07c50f82e63ed189b6c6c93
leopoldkwok <leopoldkwok@gmail.com>
2021-03-23 23:38:06 +0000

    Formspree

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -12,11 +12,16 @@ const Contact = () => {
             culpa illum fugiat ratione velit fugit, maxime deserunt. Alias,
             laboriosam accusantium.
           </p>
-          <form className='contact-form'>
+          <form
+            className='contact-form'
+            action='https://formspree.io/f/xrgrzpne'
+            method='POST'
+          >
             <input
               type='email'
               className='form-input'
               placeholder='enter email'
+              name='_replyto'
             />
             <button type='submit' className='submit-btn'>
               subscribe
